ea7c51ffde55900be2ce17dc2c10cc62acb6b02a
Andres Sainz <andresmsainz@gmail.com>
2022-02-05 15:36:32 -0500

    initial commit

---
+++ 32_Switch_Statement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _32_Switch_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch Statements!");
+            Console.WriteLine("");
+
+            Console.WriteLine("Are you ready to take over the world with C# and ASP?");
+            string answer = Console.ReadLine();
+
+            switch (answer.ToUpper())
+            {
+                case "YES":
+                    Console.WriteLine("It's about freaking time... let's do it!");
+                    break;
+                case "NO":
+                    Console.WriteLine("Let's regroup and figure something out!");
+                    break ;
+                case "MAYBE":
+                    Console.WriteLine("I can't accept that answer... how bad do you want it?");
+                    break;
+                default:
+                    Console.WriteLine("I'm not sure what you mean... but I know something has to change!");
+                    break;
+            }
+        }
+    }
+}
